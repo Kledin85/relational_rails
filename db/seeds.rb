cf0e13267e570a427a2b0d4bc59e88587f37ab92
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-12-02 09:45:17 -0600

    Adds new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-course_1 = Course.create!(name: "Kyles Course", city: "Ankeny", tee_times: 45) 
-course_2 = Course.create!(name: "Not Kyles Course", city: "Des Moines", tee_times: 50) 
+  course_1 = Course.create!(name: "Kyles Course", city: "Ankeny", tee_times: 45)
+  course_2 = Course.create!(name: "Not Kyles Course", city: "Des Moines", tee_times: 50)
+  golfer_1 = course_1.golfers.create!(name: "Kyle", adult: true, tee_time: Time.now)
+  golfer_2 = course_1.golfers.create!(name: "Wes", adult: true, tee_time: Time.now)
+  golfer_3 = course_2.golfers.create!(name: "John", adult: true, tee_time: Time.now)
